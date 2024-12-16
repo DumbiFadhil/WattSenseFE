d70b418c60a70b762885b88e040a339161d5935d
Fadhil <dumbifadhil@gmail.com>
2024-12-16 19:20:14 +0700

    style: added favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,9 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata: Metadata = {
   title: 'WattSense',
   description: 'Advanced data analysis and AI consulting platform',
+  icons: {
+    icon: "./icon.png",
+  },
 }
 
 export default function RootLayout({
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,17 +3,17 @@ import Link from 'next/link'
 export default function Home() {
   return (
     <div className="text-center">
-      <h1 className="text-4xl font-bold mb-8">Welcome to WattSense</h1>
+      <h1 className="text-4xl font-bold mb-8">Welcome to <span className="text-bg-tertiary">Watt</span><span className="text-bg-secondary">Sense</span></h1>
       <div className="grid md:grid-cols-2 gap-6 max-w-2xl mx-auto">
-        <Link 
-          href="/data-analyzer" 
+        <Link
+          href="/data-analyzer"
           className="bg-tertiary text-white p-6 rounded-lg shadow-md hover:bg-primary/90 transition duration-300 block"
         >
           <h2 className="text-2xl font-semibold mb-4">Data Analyzer</h2>
           <p>Upload and analyze your datasets with our advanced AI tools</p>
         </Link>
-        <Link 
-          href="/ai-consultant" 
+        <Link
+          href="/ai-consultant"
           className="bg-secondary text-white p-6 rounded-lg shadow-md hover:bg-primary/90 transition duration-300 block"
         >
           <h2 className="text-2xl font-semibold mb-4">AI Consultant</h2>
